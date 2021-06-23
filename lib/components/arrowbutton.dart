7d5e0e48f408da4b86eabf7cffc9aab711423f04
gboliknow <gboliknow@gmail.com>
2021-06-23 01:47:25 -0200

    first commit

---
+++ lib/components/arrowbutton.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:wallet_ui/config/colors.dart';
+import 'package:wallet_ui/config/size.dart';
+
+class ArrowButton extends StatelessWidget {
+  final EdgeInsets margin;
+  final Widget icon;
+  ArrowButton({this.margin, this.icon});
+  @override
+  Widget build(BuildContext context) {
+    var height = SizeConfig.getHeight(context);
+    var width = SizeConfig.getWidth(context);
+    return Expanded(
+      child: Container(
+        margin: margin,
+        decoration: BoxDecoration(
+            color: AppColors.primaryWhite,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: AppColors.neumorpShadow),
+        alignment: Alignment.center,
+        child: icon,
+      ),
+    );
+  }
+}
---
+++ lib/config/strings.dart
@@ -0,0 +1,8 @@
+List category = [
+  {"name": "Groceries", "amount": 80.0},
+  {"name": "Online Shopping", "amount": 100.0},
+  {"name": "Eating Out", "amount": 80.0},
+  {"name": "Books", "amount": 50.0},
+  {"name": "Subscriptions", "amount": 100.0},
+  {"name": "Drinks", "amount": 30.0},
+];
